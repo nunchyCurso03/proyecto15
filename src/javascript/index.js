ac4d71371e7f9ca5e06774532fd99442f98aa209
curso03 <you@example.com>
2024-12-05 11:35:15 +0100

    Listo para empezar a escribir jquery

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,8 +1,17 @@
-console.log('Hola mundo... por segunda vez');
+$(document).ready(function() {
+    accionParaElClick = function(event) {
+        $(this).text("Bémeme");
+    }
+
+    $("button").on("click", accionParaElClick);
+
+    console.log('Hola mundo... por tercera vez');
+});
 
 
-document.getElementById('boton').onclick = function(){
-    alert ('Me has presionado. Me voy de vacaciones a relajarme');
-}
+//document.getElementById('boton').onclick = function () {
+    //alert ('Me has presionado. Me voy de vacaciones a relajarme');
+  //  document.getElementById('boton').innerText = 'ya me has comido';
 
-console.log('Hola mundo... por tercera vez');
+//}
+ 
