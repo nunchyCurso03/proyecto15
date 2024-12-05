a9f1e436c7f931630cf03bfc3d6cb474c8015585
curso03 <you@example.com>
2024-12-05 13:02:06 +0100

    Creado ya con datos

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -16,13 +16,32 @@
 //}
 
 //generar dinámicamente una etiqueta
-$(document).ready(function(){
+/*$(document).ready(function(){
 
     solicitudes = ["Mensaje1", "Hola", "Adios"]; // array con los textos
 
     for(i = 0; i< solicitudes.length; i++){ // ciclo que recorre el array y va creando los botones
         $("#maestro").append(
             $("<li>").text(solicitudes[i])
+        );
+    }
+});  */
+
+
+//crear objeto dinámicamente. Commit Creado ya con datos
+ $(document).ready(function(){
+
+    solicitud  = {
+        
+        "id": 1,
+        "nombre": "Juan",
+        "apellido": "Secreto"
+
+    };
+    let solicitudes =[solicitud, solicitud,solicitud];
+    for(i = 0; i< solicitudes.length; i++){ // ciclo que recorre el array y va creando los botones
+        $("#maestro").append(
+            $("<li>").text(solicitudes[i].nombre + ' ' + solicitudes[i].apellido )
         );
     }
 });
